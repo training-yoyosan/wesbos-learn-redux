b6ccb9c8ec1829213be58c058e835747ff861add
Marius Palade <marius@beecoded.ro>
2021-06-07 10:55:03 +0300

    add components and routing

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div class="single-photo">
+        I'm the single
+      </div>
+    )
+  }
+})
+
+export default Single;
