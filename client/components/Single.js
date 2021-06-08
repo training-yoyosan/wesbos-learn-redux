cbcea4734fe090f98159769775404cbcb459584d
Marius Palade <marius@beecoded.ro>
2021-06-08 12:44:26 +0300

    add Single page with comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React from 'react'
+import { Link } from 'react-router'
+import Comments from './Comments'
+import Photo from './Photo'
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params
+    const i = this.props.posts.findIndex((post) => post.code === postId)
+    const post = this.props.posts[i]
+    const postComments = this.props.comments[postId] || []
+    console.log(postId, postComments, this.props.comments);
+
     return (
       <div className="single-photo">
-        I'm the single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
       </div>
     )
   }
