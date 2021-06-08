4f3cc32dd22cacfcc95d298cdf3232432d5aba00
Marius Palade <marius@beecoded.ro>
2021-06-08 12:08:59 +0300

    implement likes reducer

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -20,7 +20,7 @@ const Photo = React.createClass({
           <figcaption>
             <p>{post.caption}</p>
             <div className="control-buttons">
-              <button className="likes">&hearts; {post.likes}</button>
+              <button className="likes" onClick={this.props.increment.bind(null, i)}>&hearts; {post.likes}</button>
               <Link to={`/view/${post.code}`} className="button">
                 <span className="comment-count">
                   <span className="speech-bubble"></span>
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,3 +1,13 @@
 export default function (state = [], action) {
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index
+      return [
+        ...state.slice(0, i), // before the item we're updating
+        { ...state[i], likes: state[i].likes + 1 },
+        ...state.slice(i + 1) // after the item we're updating
+      ]
+  }
+
   return state;
 }
